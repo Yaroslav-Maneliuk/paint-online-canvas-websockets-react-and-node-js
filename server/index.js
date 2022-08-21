1f31ff6df040cffb9dc8377cb2392f14db14daf8
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-08-21 21:10:28 +0300

    Add server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
-const WSserver = require("express-ws")(app);
-
+const WSServer = require("express-ws")(app);
+const aWss = WSServer.getWss();
 const PORT = process.env.PORT || 5000;
 
-app.ws('/', (ws, req)=>{
-  console.log('Connection done')
-})
+app.ws("/", (ws, req) => {
+  console.log("Connection is done");
+  ws.send("You have successfully connected");
+  ws.on("message", (msg) => {
+    msg = JSON.parse(msg);
+    switch (msg.method) {
+      case "connection":
+        connectionHandler(ws, msg);
+        break;
+    }
+    console.log(JSON.parse(msg));
+  });
+});
 
+app.listen(PORT, () => console.log("server started on PORT ${PORT}"));
 
-app.listen(PORT, () => console.log('server started on PORT ${PORT}'));
+const connectionHandler = (ws, msg) => {
+  ws.id = msg.id;
+  broadcastConnection(ws, msg);
+};
+
+const broadcastConnection = (ws, msg) => {
+  aWss.clients.forEach((client) => {
+    if (client.id === msg.id) {
+      client.send("Users ${msg.userName} is connection");
+    }
+  });
+};
